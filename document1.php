d04a7f201ef43ee30a277a66301c89fdf87a2cce
Dehlzy <bamidelestephen0@gmail.com>
2018-04-30 14:07:51 +0100

    Initial commit

---
+++ document1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+  <head>
+  	   <LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+  	  
+  </head>
+  <body background="http://www.kinyu-z.net/data/wallpapers/61/916711.jpg">
+  	<h1>"We are Nigeria's most<br> comprehensive car website"</h1>
+  	 <h3>"Giving our users a much simpler and faster way to search for a used<br> car combined with the tools that we are building to make<br> better-informed decisions"</h3>
+   <h4 id="text1"></h4>  
+       <?php
+// Prints the day
+echo date("l") . "<br>";
+
+// Prints the day, date, month, year, time, AM or PM
+echo date("l jS \of F Y h:i:s A");
+?>
+  	<div class="image"><img src="https://www.avisprestige.com/pages/extranet/160/1456846187-Mercedes-C63-AMG.png"></div>
+
+  </body>
+  <footer>
+  	 <h5>sponsored by HNG internship 4.0</h5>
+  </footer>
+  	
+</html>
